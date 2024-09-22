748e39eab28c7c5e9e733707ddbf3ce25a7ce373
Martin Stancsics <martin.stancsics@gmail.com>
2024-09-22 10:50:44 +0200

    Make pre-commit happy

--- src/util/move_quarto_output.py
+++ src/util/move_quarto_output.py
@@ -5,8 +5,7 @@ out_dir = pathlib.Path(os.environ["QUARTO_PROJECT_OUTPUT_DIR"])
 src_dir = out_dir / "src"
 output_files = src_dir.glob("presentation/*.html")
 output_files = [
-    pathlib.Path(file) for file
-    in os.environ["QUARTO_PROJECT_OUTPUT_FILES"].split()
+    pathlib.Path(file) for file in os.environ["QUARTO_PROJECT_OUTPUT_FILES"].split()
 ]
 for output_file in output_files:
     output_file.rename(out_dir / output_file.name)
